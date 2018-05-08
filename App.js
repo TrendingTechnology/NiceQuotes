2a7df058cf3102b9a03633ec5d9fcc86bddb3589
Erik Behrends <behrends@users.noreply.github.com>
2018-05-08 07:11:09 +0200

    Komponente erklärt

--- App.js
+++ App.js
@@ -1,23 +1,29 @@
-import React from 'react';
+// import-Anweisungen
+import React, { Component } from 'react'; // wichtig: React Native basiert auf React
 import { StyleSheet, Text, View } from 'react-native';
+// APIs und Komponenten
+// eigene Komponenten...
 
-export default class App extends React.Component {
+// Komponente als Klasse
+export default class App extends Component {
   render() {
+    // Darstellung im UI
     return (
+      // JSX
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
 }
 
+// Styles für Aussehen und Layout
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
