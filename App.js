b626af4ea55886f2f0308a38285739910697fa1c
Erik Behrends <behrends@users.noreply.github.com>
2018-05-11 13:10:33 +0200

    Lokale Zitate

--- App.js
+++ App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
+const data = [
+  {
+    text:
+      'Probleme kann man niemals mit derselben Denkweise lösen, durch die sie entstanden sind.',
+    author: 'Albert Einstein'
+  },
+  {
+    text:
+      'Man braucht nichts im Leben zu fürchten, man muss nur alles verstehen.',
+    author: 'Marie Curie'
+  },
+  { text: 'Nichts ist so beständig wie der Wandel.', author: 'Heraklit' }
+];
+
 export default class App extends Component {
   render() {
     return (
