699c505be786a1685953f4e9fa1684ef5b79cda7
Erik Behrends <behrends@users.noreply.github.com>
2018-07-18 12:57:36 +0200

    TextInputs einheitlich mit Rahmen darstellen

--- js/components/NewQuote.js
+++ js/components/NewQuote.js
@@ -10,6 +10,16 @@ export default class NewQuote extends Component {
         animationType="slide"
       >
         <View style={styles.container}>
+          <TextInput
+            style={styles.input}
+            placeholder="Inhalt des Zitats"
+            underlineColorAndroid="transparent"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Autor/in des Zitats"
+            underlineColorAndroid="transparent"
+          />
           <Button title="Speichern" onPress={this.props.onSave} />
         </View>
       </Modal>
@@ -22,5 +32,15 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'deepskyblue',
+    borderRadius: 4,
+    width: '80%',
+    marginBottom: 20,
+    fontSize: 20,
+    padding: 10,
+    height: 50
   }
 });
